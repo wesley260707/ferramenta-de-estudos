467addc5f22cf958237427d26e5c148657cb67ea
wesley260707 <00001077015859SP@al.educacao.sp.gov.br>
2024-11-25 11:49:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Scracht?',
+    'O Scracht é uma linguagem de programação visual que permite a criação de jogos, animações, histórias interativas e outras produções'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Itália?',
+    'A capital da França é Roma'
+)
+
+criaCartao(
+    'História',
+    'Qual é o primeiro presidente do Brasil?',
+    'Marechal Deodoro da Fonseca foi o primeiro presidente do Brasil. Militar, ele assumiu o poder depois da Proclamação da República, em 1889.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz dez em Inglês?',
+    'Dez em ingles é TEN'
+)
